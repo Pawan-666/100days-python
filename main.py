deab41778f693cbba7e9f63b86d026653a9fb309
Pawan-666 <eulogy1996@gmail.com>
2024-02-23 11:09:33 +0545

    data_types

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-a = input("a: ")
-b = input("b: ")
-print(a, b)
-x = a
-a = b
-b = x
-print(a, b)
+print("123" + "345")
+print(int("123") + int("345"))
+print(12_3 + 345)     # _ can be used as , placeholder for readability purpose
+print(123 + 345)
+print(str(123) + str(123))
+print(123 + float(123.50))
+
+# 123345
+# 468
+# 468
+# 468
+# 123123
+# 246.5
+
