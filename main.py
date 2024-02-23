985dd5d5203098736905f9bd09056c521f9e49ea
Pawan-666 <eulogy1996@gmail.com>
2024-02-23 12:00:04 +0545

    remaining time

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your wieght in kg: "))
-bmi = weight/(height ** 2)
-print(f'Your body mass index is {int(bmi)}')
+#Days, weeks, months remaining
 
+your_age = round(float(input("Enter your current age: ")),2)
+remaining_yrs = 70.00 - your_age
+remaining_days = remaining_yrs * 365
+remaining_weeks = remaining_yrs * 52
+remaining_months = remaining_yrs * 12
+message = f"You have remaining {int(remaining_days)} days {int(remaining_weeks)} weeks {int(remaining_months)} months"
+print(message)
