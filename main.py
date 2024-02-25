6c233dac6296012a76501d623ee152aeec69369c
Pawan-666 <eulogy1996@gmail.com>
2024-02-25 18:57:15 +0545

    Leap year calculator

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-print("Welcome to the tip calculator")
-initial_total = float(input("What was the total bill? $"))
-no_of_people = int(input("How many people to split the bill? "))
-tip_percentage = int(input("What percentage tip would you like to give? "))
-final_total = initial_total + tip_percentage/100*initial_total
-each_person = final_total/no_of_people
-final_amount = round(each_person,2)
-final_amount = "{:.2f}".format(final_amount)      # force 2 no.s after floating point
-print(f"Each person should pay: ${final_amount}")
+year = int(input("Which year you want to check? "))
 
-
-
+if (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0):
+    print("It is a leap year")
+else:
+    print("It isn't a leap year")
