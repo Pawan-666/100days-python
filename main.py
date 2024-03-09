5f8ae183b696333d97a90686b64171ac4697d966
Pawan-666 <eulogy1996@gmail.com>
2024-03-09 17:12:41 +0545

    modules, random_modules, custom module

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-print("Welcome to the Treasure Island, Your mission is to find the treasure")
-in_out = input("type left or right to go to that direction: ")
-if in_out.lower() == "left":
-   in_out = input("swim or wait: ") 
-   if in_out.lower() == "wait":
-       in_out = input("Choose door color, either red,blue or yellow: ")
-       if (in_out.lower() == "red") or (in_out.lower() == "blue"):
-           print("Game Over")
-       elif in_out.lower() == "yellow":
-           print("you win!")
-       else:
-           print("Enter the specified color only")
-   elif in_out.lower() == "swim":
-       print("Game Over")
-   else:
-       print("Wrong Entry")
-elif in_out.lower() == "right":
-    print("Game Over")
-else:
-    print("Wrong entry")
+import random
+import new_module         # custom module
+
+random_int_no = random.randint(1,10)      # random int
+random_float_no = random.random()        # random float between 0,1
+random_float_no1 = random.uniform(2.5,9)
+print(random_int_no)
+print(random_float_no)
+print(random_float_no1)
+
+print(new_module.pi)
