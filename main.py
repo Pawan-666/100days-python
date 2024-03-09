6bd889cfd49cfe39fb802d8bbc676f25038d9000
Pawan-666 <eulogy1996@gmail.com>
2024-03-09 17:17:35 +0545

    Heads/Tails

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import random
-import new_module         # custom module
+random_no = random.randint(1,2)
 
-random_int_no = random.randint(1,10)      # random int
-random_float_no = random.random()        # random float between 0,1
-random_float_no1 = random.uniform(2.5,9)
-print(random_int_no)
-print(random_float_no)
-print(random_float_no1)
-
-print(new_module.pi)
+if random_no == 1:
+    print("Heads")
+else:
+    print("Tails")
