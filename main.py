f1557cc0fddd1a227b7e3557cd8447e11d360a88
Pawan-666 <eulogy1996@gmail.com>
2024-03-09 19:07:55 +0545

    lists split, paying_bills

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random
-random_no = random.randint(1,2)
 
-if random_no == 1:
-    print("Heads")
-else:
-    print("Tails")
+names_string = input("Give everybody's name, seperated by a comma. ")
+names = names_string.split(", ")
+print(names)
+
+random_name = random.choice(names)
+# random_name = random.randint(0, len(names) - 1)
+print(random_name)
+# print(f"{names[random_name]} is going to pay the bill")
+print(f"{random_name} is going to pay the bill")
